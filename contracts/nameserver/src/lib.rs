45922ffacb30993ed77e4c5b442c2d8be6ecf208
PFC(Ian) <pfc-validator@protonmail.com>
2022-11-09 12:18:13 -0600

    feat: nameserver basic operation

---
+++ contracts/nameserver/src/lib.rs
@@ -0,0 +1,155 @@
+pub mod error;
+pub mod execute;
+pub mod query;
+pub mod state;
+
+pub use query::config;
+
+use crate::error::ContractError;
+use cosmwasm_std::{to_binary, Empty};
+use cw2::set_contract_version;
+use cw721_base::Cw721Contract;
+pub use cw721_base::{
+    ContractError as CW721ContractError, InstantiateMsg as CW721InstantiateMsg, MintMsg,
+    MinterResponse,
+};
+use id_shared::state::ADMIN;
+use id_types::nameserver::{Extension, NameServerExecuteMsg, NameServerQueryMsg};
+// Version info for migration
+const CONTRACT_NAME: &str = "crates.io:id-nameserver";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+pub type MintExtension = Extension;
+
+pub type NameServerContract<'a> =
+    Cw721Contract<'a, Extension, Empty, NameServerExecuteMsg, NameServerQueryMsg>;
+pub type ExecuteMsg = cw721_base::ExecuteMsg<Extension, NameServerExecuteMsg>;
+pub type QueryMsg = cw721_base::QueryMsg<NameServerQueryMsg>;
+
+pub mod entry {
+    use super::*;
+
+    use crate::query::{resolve, reverse_record, verifier, verifier_public_key, verifier_table};
+    use cosmwasm_std::entry_point;
+    use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
+    use id_shared::{blacklist, fees, ownership};
+    use id_types::nameserver::{InstantiateMsg, MigrateMsg};
+    use semver::Version;
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn instantiate(
+        mut deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        msg: InstantiateMsg,
+    ) -> Result<Response, ContractError> {
+        // Explicitly set contract name and version, otherwise set to cw721-base info
+        set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)
+            .map_err(ContractError::Std)?;
+        let cw721inst = CW721InstantiateMsg {
+            name: msg.name.clone(),
+            symbol: msg.symbol.clone(),
+            minter: msg.admin.clone(),
+        };
+        let res = NameServerContract::default().instantiate(deps.branch(), env, info, cw721inst)?;
+
+        execute::instantiate(res, deps.branch(), msg)
+    }
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn execute(
+        deps: DepsMut,
+        env: Env,
+        info: MessageInfo,
+        msg: ExecuteMsg,
+    ) -> Result<Response, ContractError> {
+        match msg {
+            ExecuteMsg::Mint(msg) => execute::mint(deps, env, info, msg),
+            ExecuteMsg::Extension { msg } => match msg {
+                NameServerExecuteMsg::TransferOwnership { new_owner, blocks } => {
+                    ownership::transfer_ownership(deps, env, info, new_owner, blocks)
+                        .map_err(ContractError::IdSharedError)
+                }
+                NameServerExecuteMsg::AcceptOwnership {} => {
+                    ownership::accept_ownership(deps, env, info)
+                        .map_err(ContractError::IdSharedError)
+                }
+                NameServerExecuteMsg::UpdateListingFee { fee } => {
+                    fees::update_listing_fee(deps, info, fee).map_err(ContractError::IdSharedError)
+                }
+                NameServerExecuteMsg::UpdateListingFeeAccount {
+                    fee,
+                    fee_account_type,
+                    fee_account,
+                } => {
+                    fees::update_listing_fee_account(deps, info, fee, fee_account_type, fee_account)
+                        .map_err(ContractError::IdSharedError)
+                }
+                NameServerExecuteMsg::VerifyName { .. } => {
+                    // TODO store signatures in a new table/Extension
+                    todo!()
+                }
+                NameServerExecuteMsg::AddVerifier {
+                    name,
+                    wallet,
+                    public_key,
+                } => execute::add_verifier_entry(deps, info, name, wallet, public_key),
+                NameServerExecuteMsg::RemoveVerifier { name } => {
+                    execute::remove_verifier_entry(deps, info, name)
+                }
+                NameServerExecuteMsg::UpdateVerifier {
+                    name,
+                    wallet,
+                    public_key,
+                } => execute::update_verifier_entry(deps, info, name, wallet, public_key),
+            },
+            _ => NameServerContract::default()
+                .execute(deps, env, info, msg)
+                .map_err(ContractError::CW721Base),
+        }
+    }
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
+        match msg {
+            QueryMsg::Extension { msg } => match msg {
+                NameServerQueryMsg::Admin {} => to_binary(&ADMIN.query_admin(deps)?),
+                NameServerQueryMsg::Config {} => to_binary(&config(deps)?),
+                NameServerQueryMsg::Blacklist { name } => {
+                    to_binary(&blacklist::query_entry(deps, name)?)
+                }
+                NameServerQueryMsg::Blacklists { start_after, limit } => {
+                    to_binary(&blacklist::query_entries(deps, start_after, limit)?)
+                }
+                NameServerQueryMsg::ReverseRecord { address } => {
+                    to_binary(&reverse_record(deps, env, address)?)
+                }
+
+                NameServerQueryMsg::Resolve { name } => resolve(deps, env, name),
+                NameServerQueryMsg::Verifier { name } => to_binary(&verifier(deps, name)?),
+                NameServerQueryMsg::Verifiers { start_after, limit } => {
+                    to_binary(&verifier_table(deps, start_after, limit)?)
+                }
+                NameServerQueryMsg::VerifierPublicKey {
+                    public_key,
+                    start_after,
+                    limit,
+                } => to_binary(&verifier_public_key(deps, public_key, start_after, limit)?),
+            },
+            _ => NameServerContract::default().query(deps, env, msg),
+        }
+    }
+
+    #[cfg_attr(not(feature = "library"), entry_point)]
+    pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
+        let old_version: Version =
+            cw_utils::ensure_from_older_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+        // do migration stuff here
+        set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+        Ok(Response::new()
+            .add_attribute("contract_name", CONTRACT_NAME)
+            .add_attribute("previous_contract_version", old_version.to_string())
+            .add_attribute("new_contract_version", CONTRACT_VERSION))
+    }
+}
---
+++ contracts/nameserver/src/query.rs
@@ -0,0 +1,111 @@
+use cosmwasm_std::{Binary, Deps, Env, Order, StdResult};
+
+use cw721_base::QueryMsg;
+use cw_storage_plus::Bound;
+use id_shared::{DEFAULT_LIMIT, MAX_LIMIT};
+
+use id_shared::state::{ADMIN, FEE, NEW_ADMIN};
+
+use crate::state::{verifiers, NAMESERVER_CONFIG};
+use crate::NameServerContract;
+use id_types::nameserver::{ConfigResponse, Extension, VerifyRecord};
+use id_types::shared::{ENSRecord, ENSResponse};
+
+pub fn config(deps: Deps) -> StdResult<ConfigResponse> {
+    let admin = ADMIN.get(deps)?;
+    let new_owner = NEW_ADMIN.may_load(deps.storage)?;
+    let fee_config = FEE.load(deps.storage)?;
+    let nameserver_config = NAMESERVER_CONFIG.load(deps.storage)?;
+    Ok(ConfigResponse {
+        owner: admin,
+        new_owner,
+        fees: fee_config,
+        renewal_blocks: nameserver_config.renewal_blocks,
+        owners_can_burn: nameserver_config.owners_can_burn,
+        owners_can_transfer: nameserver_config.owners_can_transfer,
+        verification: nameserver_config.verification,
+        verification_expiry: nameserver_config.verification_expiry,
+        suffix: nameserver_config.suffix,
+    })
+}
+
+pub fn verifier(deps: Deps, name: String) -> StdResult<VerifyRecord> {
+    verifiers().load(deps.storage, name)
+}
+
+pub fn verifier_table(
+    deps: Deps,
+    start_after: Option<String>,
+    limit: Option<u32>,
+) -> StdResult<ENSResponse<VerifyRecord>> {
+    let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
+    //    let start_addr = maybe_addr(deps.api, start_after)?;
+    let start = start_after.as_ref().map(Bound::exclusive);
+    let res = verifiers()
+        .range(deps.storage, start, None, Order::Ascending)
+        .take(limit)
+        .map(|x| x.map(|y| y.1))
+        .collect::<StdResult<Vec<VerifyRecord>>>()?;
+    Ok(ENSResponse { entries: res })
+}
+
+pub fn verifier_public_key(
+    deps: Deps,
+    public_key: String,
+    start_after: Option<String>,
+    limit: Option<u32>,
+) -> StdResult<ENSResponse<VerifyRecord>> {
+    let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
+
+    let start = start_after.as_ref().map(Bound::exclusive);
+    let res = verifiers()
+        .idx
+        .pubkey
+        .prefix(public_key)
+        .range(deps.storage, start, None, Order::Ascending)
+        .take(limit)
+        .map(|x| x.map(|y| y.1))
+        .collect::<StdResult<Vec<VerifyRecord>>>()?;
+    Ok(ENSResponse { entries: res })
+}
+
+pub fn reverse_record(deps: Deps, env: Env, address: String) -> StdResult<ENSResponse<ENSRecord>> {
+    let mut responses: Vec<ENSRecord> = vec![];
+
+    let addr_valid = deps.api.addr_validate(&address)?;
+
+    let recs: StdResult<Vec<_>> = NameServerContract::default()
+        .tokens
+        .idx
+        .owner
+        .prefix(addr_valid)
+        .range(deps.storage, None, None, Order::Ascending)
+        .take(MAX_LIMIT as usize)
+        .collect();
+    for x in recs? {
+        let meta: Extension = x.1.extension;
+
+        if meta.verified.unwrap_or_else(|| false)
+            && meta.verification_expires.unwrap_or(env.block.height + 1) > env.block.height
+            && meta.expires.unwrap_or(env.block.height + 1) > env.block.height
+        {
+            responses.push(ENSRecord {
+                name: x.0.to_string(),
+                contract: env.contract.address.clone(),
+                token_id: x.0.to_string(),
+            })
+        }
+    }
+
+    Ok(ENSResponse { entries: responses })
+}
+pub fn resolve(deps: Deps, env: Env, name: String) -> StdResult<Binary> {
+    NameServerContract::default().query(
+        deps,
+        env,
+        QueryMsg::AllNftInfo {
+            token_id: name,
+            include_expired: Some(false),
+        },
+    )
+}
